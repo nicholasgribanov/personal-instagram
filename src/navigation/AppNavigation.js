ab7d152646f1f9288ea2797b710297ae34de6c8c
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-05-24 16:50:15 +0300

    Added different header styles for IOS and Android

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,17 +1,29 @@
 import * as React from 'react';
-
+import { Platform } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { THEME } from '../theme';
 
 
 const PostNavigation = createStackNavigator();
 
 export const AppNavigation = () => {
     return (
-        <PostNavigation.Navigator>
-            <PostNavigation.Screen name='Main' component={MainScreen} options={{ title: 'Мой блог' }} />
-            <PostNavigation.Screen name='Post' component={PostScreen} options={{title:'Пост 13'}} />
+        <PostNavigation.Navigator screenOptions={{
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+            },
+            headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR
+        }}>
+            <PostNavigation.Screen
+                name='Main' component={MainScreen}
+                options={{ title: 'Мой блог' }} />
+            <PostNavigation.Screen name='Post'
+                component={PostScreen}
+                options={{
+                    title: 'Пост 13',
+                }} />
         </PostNavigation.Navigator>
     )
 }
