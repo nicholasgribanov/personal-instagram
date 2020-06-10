a062ab38b68526101b862f1171b2882c6a0322cf
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-06-10 22:24:33 +0300

    Connect to database

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,18 @@
 import * as Font from 'expo-font'
-export async function bootstrap(){
-    await Font.loadAsync({
-        'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
-        'open-regular': require('../assets/fonts/OpenSans-Regular.ttf')
-    })
+import { DB } from './db'
+export async function bootstrap() {
+
+    try {
+        await Font.loadAsync({
+            'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+            'open-regular': require('../assets/fonts/OpenSans-Regular.ttf')
+        })
+
+        await DB.init()
+        console.log('Database started...')
+    } catch (error) {
+        console.log('Error: ', error)
+    }
+
+
 }
