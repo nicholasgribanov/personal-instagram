3660c2347a8cd7c3728e3ac9cfb0097db9fa6652
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-06-11 21:23:19 +0300

    Added inserting new post into database

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ export class DB {
         return new Promise((resolve, reject) => {
             db.transaction(tx => {
                 tx.executeSql(
-                    'CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY NOT NULL, text TEXT NOT NULL, img TEXT, booked INT)',
+                    'CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY NOT NULL, text TEXT NOT NULL, img TEXT, booked INT, date TEXT)',
                     [],
                     resolve,
                     (_, error) => reject(error)
@@ -24,6 +24,19 @@ export class DB {
                     'SELECT * FROM posts',
                     [],
                     (_, result) => resolve(result.rows._array),
+                    (_, error) => reject(error)
+                )
+            })
+        })
+    }
+
+    static createPost({ img, text, date }) {
+        return new Promise((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(
+                    `INSERT INTO posts (text,booked,img,date) VALUES (?,?,?,?)`,
+                    [text, 0, img, date],
+                    (_, result) => resolve(result.insertId),
                     (_, error) => reject(error)
                 )
             })
--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,3 +1,5 @@
+import * as FileSystem from 'expo-file-system';
+
 import { BOOKING_POST, LOAD_POSTS, REMOVE_POST, ADD_POST } from "../../types"
 import { DB } from "../../db"
 
@@ -27,10 +29,24 @@ export const removePost = (id) => {
     }
 }
 
-export const addPost = post => {
-    post.id = Date.now().toString()
-    return {
-        type: ADD_POST,
-        payload: post
+export const addPost = post => async dispatch => {
+    const fileName = post.img.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+    try {
+        await FileSystem.moveAsync({
+            from: post.img,
+            to: newPath
+        })
+    } catch (e) {
+        console.log('Error: ', e)
     }
+    const payload = { ...post, img: newPath }
+    const id = await DB.createPost(payload)
+    payload.id = id
+
+    dispatch({
+        type: ADD_POST,
+        payload
+    })
+
 }
